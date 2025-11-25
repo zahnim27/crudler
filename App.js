6f9a57218aba106812dcd95579c8f5be5b8f5632
zahnim27 <forworkminhaz@gmail.com>
2025-11-25 23:45:11 +0500

    completed second video changes

--- App.js
+++ App.js
@@ -1,22 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ModuleListScreen from './src/components/screens/ModuleListScreen';
+import ModuleAddScreen from './src/components/screens/ModuleAddScreen';
+import ModuleModifyScreen from './src/components/screens/ModuleModifyScreen';
+import ModuleViewScreen from './src/components/screens/ModuleViewScreen';
+const Stack = createNativeStackNavigator();
 
-export const  App = () => {
+export const App = () => {
+  // Initialisations
+  // State
+  // Handlers
+  // View
   return (
-    <View style={styles.container}>
-      <Text>hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="ModuleListScreen"
+        screenOptions={{
+          headerStyle: { backgroundColor: 'black' },  
+          headerTintColor: 'white',                
+        }}
+      >
+        <Stack.Screen
+          name="ModuleListScreen"
+          component={ModuleListScreen}
+          options={{ title: 'List modules' }}
+        />
+        <Stack.Screen
+          name="ModuleAddScreen"
+          component={ModuleAddScreen}
+          options={{ title: 'Add modules' }}
+        />
+        <Stack.Screen
+          name="ModuleViewScreen"
+          component={ModuleViewScreen}
+          options={{ title: 'View modules' }}
+        />
+        <Stack.Screen
+          name="ModuleModifyScreen"
+          component={ModuleModifyScreen}
+          options={{ title: 'Modify modules' }}
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
---
+++ src/components/layout/Screen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+
+const Screen = ({ children }) => {
+  // Initialisations
+  // State
+  // Handlers
+  // View
+  return (
+    <View style={styles.screen}>
+      {children}
+      <StatusBar style="light" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default Screen;
