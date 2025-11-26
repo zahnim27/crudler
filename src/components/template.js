71c98ffba20cff6d321212b1432120fc79da72d9
zahnim27 <forworkminhaz@gmail.com>
2025-11-26 23:49:44 +0500

    activity 5 completed

---
+++ src/components/template.js
@@ -0,0 +1,11 @@
+import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
+const Component = () => {
+// Initialisations ---------------------
+// State -------------------------------
+// Handlers ----------------------------
+// View --------------------------------
+
+};
+const styles = StyleSheet.create({
+});
+export default Component;
