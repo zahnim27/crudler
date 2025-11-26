c87f65476b655a67b16f7cee216969219284aaf0
zahnim27 <minhaaaz27@gmail.com>
2025-11-26 23:50:28 +0500

    activity 5 completed (#3)
    Co-authored-by: zahnim27 <forworkminhaz@gmail.com>

---
+++ src/components/UI/RenderCount.js
@@ -0,0 +1,29 @@
+import React, { useRef } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const RenderCount = ({ backgroundColor = 'dodgerblue', fontColor = 'white' }) => {
+  // Initialisations ------------------------------
+    // State ----------------------------------------
+
+  const renderCount = useRef(0);
+  renderCount.current += 1;
+
+  // Handlers -------------------------------------
+
+  // View -----------------------------------------
+  return (
+   
+      <Text style={[styles.count, { backgroundColor: backgroundColor, color: fontColor }]}>
+        Number of renders: {renderCount.current}
+      </Text>
+
+  );
+};
+
+const styles = StyleSheet.create({
+  count: {
+    padding: 5,
+  },
+});
+
+export default RenderCount;
