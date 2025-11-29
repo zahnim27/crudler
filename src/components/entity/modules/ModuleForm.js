40331cf36a79df4d77ee70ebd6b0864fca056f3f
zahnim27 <forworkminhaz@gmail.com>
2025-11-29 18:39:33 +0500

    completed activty 11

---
+++ src/components/entity/modules/ModuleForm.js
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import Icons from "../../UI/Icons.js";
+import Form from "../../UI/Form.js";
+const defaultModule = {
+  ModuleID: null,
+  ModuleCode: null,
+  ModuleName: null,
+  ModuleLevel: null,
+  ModuleLeaderID: null,
+  ModuleLeaderName: null,
+  ModuleImage: null,
+};
+
+const ModuleForm = ({onSubmit, onCancel}) => {
+  // Initialisations ------------------------------
+  defaultModule.ModuleID = Math.floor(100000 + Math.random() * 900000);
+  defaultModule.ModuleImage =
+    "https://images.freeimages.com/images/small-previews/cf5/cellphone-1313194.jpg";
+  const levels = [
+    { value: 3, label: "3 (Foundation)" },
+    { value: 4, label: "4 (First year)" },
+    { value: 5, label: "5 (Second year)" },
+    { value: 6, label: "6 (Final year)" },
+    { value: 7, label: "7 (Masters)" },
+  ];
+
+  // State ----------------------------------------
+  const [module, setModule] = useState(defaultModule);
+
+  const submitLabel = "Add";
+  const submitIcon = <Icons.Add />;
+  // Handlers -------------------------------------
+
+  const handleChange = (field, value) =>
+    setModule({ ...module, [field]: value });
+  const handleSubmit = () => onSubmit(module);
+  // View -----------------------------------------
+  return (
+    <Form
+      onSubmit={handleSubmit}
+      onCancel={onCancel}
+      submitLabel={submitLabel}
+      submitIcon={submitIcon}
+    >
+      <Form.InputText
+        label="Module code"
+        value={module.ModuleCode}
+        onChange={(value) => handleChange("ModuleCode", value)}
+      />
+
+      <Form.InputText
+        label="Module name"
+        value={module.ModuleName}
+        onChange={(value) => handleChange("ModuleName", value)}
+      />
+
+      <Form.InputSelect
+        label="Module level"
+        prompt="Select module level ..."
+        options={levels}
+        value={module.ModuleLevel}
+        onChange={(value) => handleChange("ModuleLevel", value)}
+      />
+
+      <Form.InputText
+        label="Module leader"
+        value={module.ModuleLeaderName}
+        onChange={(value) => handleChange("ModuleLeaderName", value)}
+      />
+
+      <Form.InputText
+        label="Module image URL"
+        value={module.ModuleImage}
+        onChange={(value) => handleChange("ModuleImage", value)}
+      />
+    </Form>
+  );
+};
+
+// Styles -----------------------------------------
+const styles = StyleSheet.create({
+  item: {
+    marginBottom: 10,
+  },
+  itemLabel: {
+    fontSize: 14,
+    color: "grey",
+    marginBottom: 4,
+  },
+  itemInput: {
+    borderWidth: 1,
+    borderColor: "grey",
+    borderRadius: 5,
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    fontSize: 16,
+  },
+});
+
+export default ModuleForm;
